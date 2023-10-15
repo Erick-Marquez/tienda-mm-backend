124500ad48282da63115d0a369f08dfa152c9ac3
Erick <erickmg_13@hotmail.com>
2023-10-15 18:01:20 -0500

    purchases 0.1

---
+++ database/migrations/2023_09_24_121314_create_purchase_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_details', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('quantity');
+            $table->unsignedDecimal('price', 12,2);
+
+            $table->unsignedDecimal('total', 12,2)->nullable();
+
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('purchase_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_details');
+    }
+};
